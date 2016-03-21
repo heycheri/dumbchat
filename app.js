d9fa33be0fcd924a725a9e8c0cbf6f9b188c7270
Cheri Macale <heycheri@gmail.com>
2016-03-21 15:44:59 -0700

    Working prototype.

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ var $ = function(selector) {
 // save a reference to the messages ul element
 var messages = $('#messages');
 
+function addMessage(message) {
+  // create a new li element to hold our new message
+  var el = document.createElement('li');
+
+  // add message to li element's body
+  el.innerHTML = message;
+
+  // append message to list of messages
+  messages.appendChild(el)
+}
+
 // save a reference to the chat input element
 var chatInput = $('#chat-input')
+var chatForm  = $('#chat-form')
 
-// create a new li element to hold our new message
-var message = document.createElement('li');
-
-// add message to li element's body
-message.innerHTML = 'Message 4';
-
-// append message to list of messages
-messages.appendChild(message)
+chatForm.addEventListener('submit', function(event) {
+  addMessage(chatInput.value)
+  chatInput.value = "";
+  event.preventDefault()
+})
